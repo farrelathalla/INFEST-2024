e014ef7d6b0f0219f57b6197abb43f746fef67f9
AtqiyaHaydar <ashford1524@gmail.com>
2024-07-02 13:37:53 +0700

    feat: home metadata

--- app/layout.js
+++ app/layout.js
@@ -29,20 +29,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Join INFEST 2024, the biggest Investment Festival in Indonesia, organized by KSEP ITB. Learn about investment and capital market through seminars, workshops, and competitions." />
-        <meta name="keywords" content="Investment, Festival, INFEST 2024, KSEP ITB, Indonesia, Seminar, Workshop, Competition" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/logo.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;700&family=Nunito:wght@300;700;900&display=swap" rel="stylesheet" />
-        <title>INFEST 2024 | Investment Festival by KSEP ITB</title>
-      </head>
       <body className="poppins-regular">
         <Navbar />
         <div className="my-[120px] transition-all">
